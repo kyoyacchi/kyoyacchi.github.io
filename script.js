b3e1658afff7d80aa1b1cfe7ece8ac5faaecd1b9
Kyoや <63583961+kyoyacchi@users.noreply.github.com>
2025-04-19 21:54:13 +0300

    update script.js

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ async function updateDiscordPfp() {
 
     // Rastgele metinler dizisi 
     const texts = [
-        'Now, you shall perish!',
+       'Now, you shall perish!',
         'There is no escape!',
         'Inazuma shines eternal!',
         'Inabikari, sunawachi Eien nari',
