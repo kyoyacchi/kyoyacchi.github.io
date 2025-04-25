59e3973b03a48d06f7b724dacd34d49860d7ab03
Kyoや <63583961+kyoyacchi@users.noreply.github.com>
2025-04-25 20:17:49 +0300

    update script.js

--- script.js
+++ script.js
@@ -607,7 +607,8 @@ function initializeDynamicBanner() {
         'https://files.catbox.moe/4atada.jpg',
         'https://files.catbox.moe/gc1qh3.jpg',
         'https://files.catbox.moe/ph6zj4.jpeg',
-        'https://files.catbox.moe/ox23f5.jpeg'
+        'https://files.catbox.moe/ox23f5.jpeg',
+        'https://files.catbox.moe/zllsq7.jpg'
     ];
     // ----------------------------------
 
